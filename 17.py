4a45910ab668987aadb6166359135848235808d2
dariar25 <55868012+dariar25@users.noreply.github.com>
2019-11-04 20:57:47 +0500

    Add files via upload

---
+++ 17.py
@@ -0,0 +1,20 @@
+import math
+A=7.0
+H=5.0
+R=3.0
+M=12.0
+CV=A**3
+CYV=math
+.pi * R**2 * H
+if CV&gt;M:
+ print ("possibly")
+else:
+ print ("unpossibly")
+if CYV&gt;M:
+ print ("possibly")
+else:
+ print ("unpossibly")
+if M&lt;CYV&lt;CV:
+ print ("possibly")
+else:
+ print ("unpossibly")
