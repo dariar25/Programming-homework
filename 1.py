64a7380c6f6c9a2cd8dddb73b3a6ee5b1f9f5052
dariar25 <55868012+dariar25@users.noreply.github.com>
2019-11-04 13:41:57 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+Python 2.7.17 (v2.7.17:c2f86d86e6, Oct 19 2019, 21:01:17) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> T="2009-06-15"
+date=T.split ("-")
+print (date)
+print (date[2] + "." +  date [1] + "." + date[0])
+>>> 
