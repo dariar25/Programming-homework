9dbc8600ab66e06c99c27091483567446a4b6ce2
dariar25 <55868012+dariar25@users.noreply.github.com>
2019-11-05 22:34:57 +0500

    Add files via upload

---
+++ 22.py
@@ -0,0 +1,15 @@
+import random
+import math
+A = random.randint (1,50)
+B = random.randint (1,50)
+C = random.randint (1,50)
+if A &lt; B &lt; C :
+    print ("vipolnyaetsya","A &lt; B &lt; C")
+else :
+    print ("ne vipolnyaetsya","A &lt; B &lt; C") if A &gt; B &gt; C:
+    print ("vipolnyaetsya","A &gt; B &gt; C")
+else:
+    print ("ne vipolnyaetsya","A &gt; B &gt; C")
+print ("A -",A)
+print ("B -",B)
+print ("C -",C)
