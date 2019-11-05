26b8e684b3af4685be6e5b7670358b2c110e7838
dariar25 <55868012+dariar25@users.noreply.github.com>
2019-11-05 22:27:17 +0500

    Delete 19.py

--- 19.py
+++
@@ -1,6 +0,0 @@
-Python 2.7.17 (v2.7.17:c2f86d86e6, Oct 19 2019, 21:01:17) [MSC v.1500 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a=11.0
-b=7.0
-c=13.0
-if a+b&gt;c and a-b
