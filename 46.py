d1263d173645365ade825cfd0cfe11fa6d66484a
dariar25 <55868012+dariar25@users.noreply.github.com>
2019-12-25 00:30:12 +0500

    Add files via upload

---
+++ 46.py
@@ -0,0 +1,33 @@
+import random
+
+N = 11
+T = random.randint(0, 100)
+
+
+def init_random_list(list_size):
+    list = []
+    while list_size &gt; 0:
+        list.append(random.randint(-3, 3))
+        list_size = list_size - 1
+
+    return list
+
+
+lst = init_random_list(N)
+count_positive = 0
+print("Исходный массив:        ", lst)
+print("Число T:", T)
+
+for element in lst:
+    if element &gt;= 0:
+        count_positive += 1
+
+i = 0
+while i &lt; len(lst): if lst[i] &gt;= 0:
+        lst[i] += T/count_positive
+
+    i += 1
+
+print("Количество положительных чисел:", count_positive)
+print("Доля которая прибавляется к каждому из них:", T/count_positive)
+print("Массив после модификации:", lst)
