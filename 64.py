77c07ead6171428357dcf29ffa81123ccc8d54cc
dariar25 <55868012+dariar25@users.noreply.github.com>
2019-12-27 00:13:09 +0500

    Add files via upload

---
+++ 64.py
@@ -0,0 +1,22 @@
+import re
+list_numbers = []
+
+sum = 0
+
+while True:
+    print("Введите число:", ends=' ')
+    string = re.sub(r'[^0-9\-]+', '', input())
+    if len(string) == 0:
+        print("В строке не обнаружены числа")
+
+        number = int(string)
+        list_numbers.append(number)
+
+        if number == 99999:
+            break
+        elif number == 0:
+            print("Сумма:", sum)
+            else:
+                sum += number
+
+        print("Сумма:", sum)
