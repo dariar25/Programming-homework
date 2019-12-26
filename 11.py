3637bf6589974e0d933ec45d3d826b109a6c7e2e
dariar25 <55868012+dariar25@users.noreply.github.com>
2019-12-26 23:44:53 +0500

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,13 @@
+A =8.0
+B =4.0
+C =3.0
+M =4.0
+K =5.0
+if (A<M and B<K)or(A<K and B<M):
+ print("korobka proidet")
+elif (B<M and C<K) or (B<K and C<M):
+ print("korobka proidet")
+elif (A<M and C<K) or (A<K and C<M):
+ print("korobka proidet")
+else:
+ print("korobka ne proidet")
